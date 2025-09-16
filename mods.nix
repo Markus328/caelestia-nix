4a358b7b7fd7d7b18f7486f3399f19d33623858d
Markus328 <markus328@tutanota.com>
2025-09-15 21:56:37 -0300

    mods.nix: unified mkMod functions in generic one
    now it also passes the  attr to the called module

--- mods.nix
+++ mods.nix
@@ -1,51 +1,60 @@
-{lib}: {
-  mkMod = path: ({
-    config,
-    lib,
-    pkgs,
-    ...
-  }: let
-    mod = lib.getAttrFromPath path config.programs.caelestia-dots;
-    mod_name = lib.showOption path;
-    mod_path = lib.path.append ./configs (lib.path.subpath.join path);
-    default = import (lib.path.append mod_path "config.nix") {inherit config lib mod;};
-  in {
-    imports = [(import mod_path {inherit mod path;})];
-    options.programs.caelestia-dots = lib.setAttrByPath path (with lib; {
-      enable = mkEnableOption "Enable Caelestia ${mod_name} module";
-      settings = mkOption {
-        type = types.attrsOf types.anything;
-        description = "Caelestia ${mod_name} module settings";
-        inherit default;
-        apply = userSettings: lib.recursiveUpdate default userSettings;
-      };
-      extraConfig = mkOption {
-        type = types.str;
-        description = "Caelestia ${mod_name} module extra config";
-        default = "";
-      };
+{lib}: let
+  mods = rec {
+    _make_module = {
+      path,
+      root ? ./configs,
+      raw ? false,
+    }: ({
+      config,
+      lib,
+      pkgs,
+      ...
+    }: let
+      mod = lib.getAttrFromPath path config.programs.caelestia-dots;
+      mod_name = lib.showOption path;
+      mod_path = lib.path.append root (lib.path.subpath.join path);
+      default = import (lib.path.append mod_path "config.nix") {inherit config lib mod;};
+    in {
+      imports = [(import mod_path {inherit mod path mods;})];
+      options.programs.caelestia-dots = lib.setAttrByPath path (with lib;
+        if raw
+        then
+          (mkOption {
+            type = types.attrsOf types.anything;
+            description = "Caelestia ${mod_name} module";
+            inherit default;
+            apply = userSettings: lib.recursiveUpdate default userSettings;
+          })
+        else {
+          enable = mkEnableOption "Enable Caelestia ${mod_name} module";
+          settings = mkOption {
+            type = types.attrsOf types.anything;
+            description = "Caelestia ${mod_name} module settings";
+            inherit default;
+            apply = userSettings: lib.recursiveUpdate default userSettings;
+          };
+          extraConfig = mkOption {
+            type = types.str;
+            description = "Caelestia ${mod_name} module extra config";
+            default = "";
+          };
+        });
     });
-  });
-  mkPassMod = from: to: lib.mkAliasOptionModule (["programs" "caelestia-dots"] ++ from) to;
 
-  mkRawMod = path: ({
-    config,
-    lib,
-    pkgs,
-    ...
-  }: let
-    mod = lib.getAttrFromPath path config.programs.caelestia-dots;
-    mod_path = lib.path.append ./configs (lib.path.subpath.join path);
-    mod_name = lib.showOption path;
-    default = import (lib.path.append mod_path "config.nix") {inherit config lib mod;};
-  in {
-    imports = [(import mod_path {inherit mod path;})];
-    options.programs.caelestia-dots = lib.setAttrByPath path (with lib;
-      mkOption {
-        type = types.attrsOf types.anything;
-        description = "Caelestia ${mod_name} module";
-        inherit default;
-        apply = userSettings: lib.recursiveUpdate default userSettings;
-      });
-  });
-}
+    mkMod = path: _make_module {inherit path;};
+    mkRawMod = path:
+      _make_module {
+        inherit path;
+        raw = true;
+      };
+
+    mkPassMod = from: to: lib.mkAliasOptionModule (["programs" "caelestia-dots"] ++ from) to;
+
+    mkMultipleMods = {
+      args ? {},
+      parent,
+    }: paths:
+      map (path: _make_module (args // {path = parent ++ path;})) paths;
+  };
+in
+  mods
