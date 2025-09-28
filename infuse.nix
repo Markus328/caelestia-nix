600118ec815b965e23f4ed67584db5e9420721d4
Markus328 <markus328@tutanota.com>
2025-09-28 18:43:54 -0300

    infuse.nix: add __remove and __replace sugars allow easier overrides on lists

--- infuse.nix
+++ infuse.nix
@@ -1,11 +1,36 @@
 {lib}: let
-  infuse =
-    (import
-      (builtins.fetchGit {
-        url = "https://codeberg.org/amjoseph/infuse.nix";
-        name = "infuse.nix";
-        rev = "c8fb7397039215e1444c835e36a0da7dc3c743f8";
-      }) {inherit lib;}).v1.infuse;
+  _infuse =
+    import
+    (builtins.fetchGit {
+      url = "https://codeberg.org/amjoseph/infuse.nix";
+      name = "infuse.nix";
+      rev = "c8fb7397039215e1444c835e36a0da7dc3c743f8";
+    }) {
+      inherit lib;
+      sugars = let
+        ifList = infusion: target: f:
+          if lib.isList infusion && lib.isList target
+          then f infusion target
+          else throw "${infusion} or ${target} is of type list";
+      in
+        _infuse.v1.default-sugars
+        ++ lib.attrsToList {
+          __remove = path: remove: target: lib.filter (tg: !lib.any (r: lib.hasPrefix r tg) remove) target; # Remove all strings starting with any remove list strings.
+          __replace = path: replace: target:
+          # Replace all strings from replacement list to the last one in each list.
+            with lib.lists; let
+              froms = flatten (map (frs: dropEnd 1 frs) replace);
+              tos = flatten (map (ts: replicate (builtins.length ts - 1) (last ts)) replace);
+            in
+              map (
+                tg:
+                  builtins.replaceStrings froms tos tg
+              )
+              target;
+        };
+    };
+
+  inherit (_infuse.v1) infuse;
 
   # This function will wrap the settings to be used as infusion, allowing a lib.modules like experience
   # with infuse.nix bonus.
@@ -14,9 +39,7 @@
       if lib.isAttrs value
       then let
         attrNames = builtins.attrNames value;
-      in
-        (builtins.length attrNames == 1)
-        && (lib.hasPrefix "__" (builtins.head attrNames))
+      in (lib.hasPrefix "__" (builtins.head attrNames))
       else false;
 
     transformToInfusion = set:
