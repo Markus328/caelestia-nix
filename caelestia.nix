8c0d92254c4432328a46370cedecfa508ebee720
Markus328 <markus328@tutanota.com>
2025-09-28 22:17:14 -0300

    caelestia.nix: add caelestia and hypr modules

--- caelestia.nix
+++ caelestia.nix
@@ -7,19 +7,21 @@ inputs: {
   cfg = config.programs.caelestia-dots;
   mods = import ./mods.nix {inherit lib;};
 in {
-  imports = with mods; [
-    inputs.caelestia-shell.homeManagerModules.default
-    (mkPassMod ["shell"] ["programs" "caelestia"])
-    (mkPassMod ["cli"] ["programs" "caelestia" "cli"])
-  ];
+  imports = with mods;
+    [
+      inputs.caelestia-shell.homeManagerModules.default
+      # (mkMod ["caelestia" "shell"])
+      # (mkMod ["caelestia" "cli"])
+    ]
+    ++ (mkMultipleMods {parent = [];} [
+      "hypr"
+      ["caelestia" "cli"]
+      ["caelestia" "shell"]
+    ]);
 
   options = with lib; {
     programs.caelestia-dots = {
       enable = mkEnableOption "Enable Caelestia dotfiles";
     };
-  };
-
-  config = lib.mkIf cfg.enable {
-    programs.caelestia.enable = lib.mkDefault true;
   };
 }
