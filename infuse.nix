4575a11bfe1fe07a373b57befb73bb45ea7a3bb1
Markus328 <markus328@tutanota.com>
2025-11-21 00:37:09 -0300

    infuse.nix: fix wrapToInfusion for invalid attrs
    that makes possible to deal with packages or any set containing special (lib.hasPrefix __) with non-special names

--- infuse.nix
+++ infuse.nix
@@ -1,4 +1,20 @@
 {lib}: let
+  sugars =
+    _infuse.v1.default-sugars
+    ++ lib.attrsToList {
+      __remove = path: remove: target: lib.filter (tg: !lib.any (r: lib.hasPrefix r tg) remove) target; # Remove all strings starting with any remove list strings.
+      __replace = path: replace: target:
+      # Replace all strings from replacement list to the last one in each list.
+        with lib.lists; let
+          froms = flatten (map (frs: dropEnd 1 frs) replace);
+          tos = flatten (map (ts: replicate (builtins.length ts - 1) (last ts)) replace);
+        in
+          map (
+            tg:
+              builtins.replaceStrings froms tos tg
+          )
+          target;
+    };
   _infuse =
     import
     (builtins.fetchGit {
@@ -6,23 +22,7 @@
       name = "infuse.nix";
       rev = "c8fb7397039215e1444c835e36a0da7dc3c743f8";
     }) {
-      inherit lib;
-      sugars =
-        _infuse.v1.default-sugars
-        ++ lib.attrsToList {
-          __remove = path: remove: target: lib.filter (tg: !lib.any (r: lib.hasPrefix r tg) remove) target; # Remove all strings starting with any remove list strings.
-          __replace = path: replace: target:
-          # Replace all strings from replacement list to the last one in each list.
-            with lib.lists; let
-              froms = flatten (map (frs: dropEnd 1 frs) replace);
-              tos = flatten (map (ts: replicate (builtins.length ts - 1) (last ts)) replace);
-            in
-              map (
-                tg:
-                  builtins.replaceStrings froms tos tg
-              )
-              target;
-        };
+      inherit lib sugars;
     };
 
   inherit (_infuse.v1) infuse;
@@ -30,28 +30,30 @@
   # This function will wrap the settings to be used as infusion, allowing a lib.modules like experience
   # with infuse.nix bonus.
   wrapSettingsToInfusion = let
-    isSugar = value:
-      if lib.isAttrs value
-      then let
-        attrNames = builtins.attrNames value;
-      in
-        (builtins.length attrNames > 0) && (lib.hasPrefix "__" (builtins.head attrNames))
-      else false;
+    isSugar = name: lib.any (s: s.name == name) sugars;
+    isSpecial = lib.hasPrefix "__";
 
-    transformToInfusion = set:
-      if (lib.isFunction set) || isSugar set
-      then set
-      else if lib.isAttrs set
-      then lib.mapAttrs (name: value: transformToInfusion value) set
-      else
-        # allow ommiting __append or __assign on leafs
-        if lib.isList set
-        then {__append = set;}
-        else {__assign = set;};
+    # an attrs is valid if has no sugars or has only sugars
+    isValidAttrs = value:
+      with lib; let
+        attrNames = builtins.attrNames value;
+        sugarCount = count isSugar attrNames;
+        hasSpecial = any isSpecial attrNames;
+      in
+        isAttrs value && ((sugarCount == 0 && !hasSpecial) || sugarCount == lists.length attrNames);
+
+    transformToInfusion = name: value:
+      if (lib.isFunction value) || (isSugar name && !lib.isAttrs value) || name == "__assign"
+      then value
+      else if isValidAttrs value
+      then lib.mapAttrs (n: v: transformToInfusion n v) value
+      else if lib.isList value
+      then {__append = value;}
+      else {__assign = value;};
   in
     transformToInfusion;
 in
   default: userSettings:
     if default != userSettings && userSettings != {}
-    then infuse default (wrapSettingsToInfusion userSettings)
+    then infuse default (wrapSettingsToInfusion "" userSettings)
     else default
